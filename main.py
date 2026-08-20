59cf29086dd9d5b6888e3c2cd643e1afb72d1a2a
stoppo22 <emastopponi@gmail.com>
2026-08-20 16:06:35 +0200

    Add command-line video paths

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import argparse
+
 from pipeline import analyze_video
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Analyze a tennis video and generate an annotated output video."
+    )
+    parser.add_argument(
+        "--input",
+        default="input_videos/input_video.mp4",
+        help="Path to the input tennis video.",
+    )
+    parser.add_argument(
+        "--output",
+        default="output_videos/output_video.avi",
+        help="Path where the annotated video will be saved.",
+    )
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+
     analyze_video(
-        input_video_path="input_videos/input_video.mp4",
-        output_video_path="output_videos/output_video.avi",
+        input_video_path=args.input,
+        output_video_path=args.output,
     )
 
 
